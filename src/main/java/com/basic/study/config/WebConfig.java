4f44db2a556fec4c26d1c5eb7242502d618eedd3
jiyunio <jiyun3269@naver.com>
2024-11-18 17:37:36 +0900

    [Refactor] cors method 추가

--- src/main/java/com/basic/study/config/WebConfig.java
+++ src/main/java/com/basic/study/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:" + port)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 }
